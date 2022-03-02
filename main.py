61f0feb66720405ed2338ede6746a4fb8202ec5d
Tomi Asikainen <57344483+livingA6@users.noreply.github.com>
2022-03-02 09:14:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ for single_date in daterange(start_date, end_date):
         data = pandas.json_normalize(r.json())
         df = pandas.DataFrame(data)
         print(df.values)
-        df.to_csv(path_or_buf= '/Users/dstar/Projects/PycharmProjects/DigiTraffic/train_4.csv', sep=';',header=False,mode='a')
+        df.to_csv(path_or_buf= '/Users/dstar/Projects/PycharmProjects/DigiTraffic/train_4_july_2020.csv', sep=';',header=False,mode='a')
         #df.to_csv(path_or_buf='<path to csv-file>', sep=';', header=False,mode='a')
     except requests.exceptions.RequestException as e:
         #print(e)
