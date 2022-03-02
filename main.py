f0180e9f3f4339999a6e1ee7a72410d06e8c2593
Tomi Asikainen <57344483+livingA6@users.noreply.github.com>
2022-03-02 08:57:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,13 @@ start_date = date(2020, 7, 1)
 end_date = date(2020, 8, 1)
 for single_date in daterange(start_date, end_date):
     URL = 'https://rata.digitraffic.fi/api/v1/trains/' + single_date.strftime("%Y-%m-%d" +'/4')
-    headers = {'Digitraffic-User': 'VR:livingA6'}
-    r = requests.get(URL, headers=headers)
-    data = pandas.json_normalize(r.json())
-    df = pandas.DataFrame(data)
-    #print(df.values)
-    #df.to_csv(path_or_buf= '/Users/dstar/Projects/PycharmProjects/DigiTraffic/Task1.csv', sep=';',header=False,mode='a')
-    df.to_csv(path_or_buf='<path to csv-file>', sep=';', header=False,mode='a')
-
-
-
-
-
-
-
-
+    headers = {'Digitraffic-User': 'livingA6'}
+    try:
+        r = requests.get(URL, headers=headers)
+        data = pandas.json_normalize(r.json())
+        df = pandas.DataFrame(data)
+        print(df.values)
+        df.to_csv(path_or_buf= '/Users/dstar/Projects/PycharmProjects/DigiTraffic/train_4.csv', sep=';',header=False,mode='a')
+        #df.to_csv(path_or_buf='<path to csv-file>', sep=';', header=False,mode='a')
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
