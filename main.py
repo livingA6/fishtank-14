d4d8f25f77c5b9ab4e174865d3d735c7bb73ba2a
Tomi Asikainen <57344483+livingA6@users.noreply.github.com>
2022-03-02 08:44:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ start_date = date(2020, 7, 1)
 end_date = date(2020, 8, 1)
 for single_date in daterange(start_date, end_date):
     URL = 'https://rata.digitraffic.fi/api/v1/trains/' + single_date.strftime("%Y-%m-%d" +'/4')
-    headers = {'Digitraffic-User': 'livingA6/Sanoma:DaDe:Task 1'}
+    headers = {'Digitraffic-User': 'VR:livingA6'}
     r = requests.get(URL, headers=headers)
     data = pandas.json_normalize(r.json())
     df = pandas.DataFrame(data)
-    #print(df)
+    #print(df.values)
     #df.to_csv(path_or_buf= '/Users/dstar/Projects/PycharmProjects/DigiTraffic/Task1.csv', sep=';',header=False,mode='a')
     df.to_csv(path_or_buf='<path to csv-file>', sep=';', header=False,mode='a')
 
